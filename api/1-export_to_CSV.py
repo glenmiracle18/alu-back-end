31cee429f6bdad293fcb1d50dd68eb0c3eb9aea7
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-02 12:59:37 +0200

    done

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,22 +15,19 @@ if __name__ == "__main__":
     user_response = requests.get(user_data_url)
     todo_response = requests.get(todo_url)
 
-    # if todo_response.status_code & user_response.status_code == 200:
     todos = todo_response.json()
     users = user_response.json()
 
     order = ['userId', 'username', 'completed', 'title']
-
     for user in users:
         if user.get("id") == employee_Id:
             employee_name = user.get("username")
 
-with open(f'{employee_Id}.csv', 'a', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile,
-                            fieldnames=order, quoting=csv.QUOTE_ALL)
-
-    for todo in todos:
-        if todo.get("userId") == employee_Id:
-            del todo['id']
-            todo['username'] = user['username']
-            writer.writerow(todo)
+    with open(f'{employee_Id}.csv', 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile,
+                                fieldnames=order, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            if todo.get("userId") == employee_Id:
+                del todo['id']
+                todo['username'] = user['username']
+                writer.writerow(todo)
